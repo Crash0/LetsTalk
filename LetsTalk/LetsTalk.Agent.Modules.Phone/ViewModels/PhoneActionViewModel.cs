14224dcffcbb7b929031abc3e13cdeee170a9089
Jonas Viktor fjeld <jvf_94@live.no>
2016-07-21 20:22:40 +0200

    Added vm for actions in phone

---
+++ LetsTalk/LetsTalk.Agent.Modules.Phone/ViewModels/PhoneActionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Input;
+using Prism.Mvvm;
+
+namespace LetsTalk.Agent.Modules.Phone
+{
+    public class PhoneActionViewModel : BindableBase
+    {
+        public ICommand Command { get; set; }
+        public string DisplayName;
+
+    }
+}
