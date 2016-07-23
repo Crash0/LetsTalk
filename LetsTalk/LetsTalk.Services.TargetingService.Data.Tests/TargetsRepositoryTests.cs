aeac51a1db80b8af31217e4020a71c94ec57198a
Jonas Viktor fjeld <jvf_94@live.no>
2016-07-23 18:43:01 +0200

    added target entity

--- LetsTalk/LetsTalk.Services.TargetingService.Data.Tests/TargetsRepositoryTests.cs
+++ LetsTalk/LetsTalk.Services.TargetingService.Data.Tests/TargetsRepositoryTests.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LetsTalk.Business.Entities.Targeting;
+using Xunit;
 
 namespace LetsTalk.Services.TargetingService.Data.Tests
 {
     public class TargetsRepositoryTests
     {
-        var targets = new List<Target>();
+        [Fact]
+        public void GetTargetById_should_returnCorectUser()
+        {
+            
+        }   
     }
 }
