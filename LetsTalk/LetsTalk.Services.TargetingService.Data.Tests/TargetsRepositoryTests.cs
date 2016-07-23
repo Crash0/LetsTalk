82196f7c1e81903c43babde6a2e7e9048ee36b1a
Jonas Viktor fjeld <jvf_94@live.no>
2016-07-23 18:36:59 +0200

    added tARGETING SERVICE AND TEST PROJS

---
+++ LetsTalk/LetsTalk.Services.TargetingService.Data.Tests/TargetsRepositoryTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsTalk.Services.TargetingService.Data.Tests
+{
+    public class TargetsRepositoryTests
+    {
+        var targets = new List<Target>();
+    }
+}
