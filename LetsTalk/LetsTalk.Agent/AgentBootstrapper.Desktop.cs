2e447001d93b474460c1c8384b2ad785240d55f3
Jonas Viktor fjeld <jvf_94@live.no>
2016-11-28 22:03:10 +0100

    StyleCopping

--- LetsTalk/LetsTalk.Agent/AgentBootstrapper.Desktop.cs
+++ LetsTalk/LetsTalk.Agent/AgentBootstrapper.Desktop.cs
@@ -1,14 +1,34 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AgentBootstrapper.Desktop.cs" company="GoDialog">
+//   All Rights Reserved
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
 using Prism.Logging;
 
 namespace LetsTalk.Agent
 {
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    ///     TODO The agent bootstrapper.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "Reviewed. Suppression is OK here.")]
     public partial class AgentBootstrapper
     {
-        private readonly EnterpriseLibraryLoggerAdapter _logger = new EnterpriseLibraryLoggerAdapter();
+        /// <summary>
+        ///     TODO The _logger.
+        /// </summary>
+        private readonly EnterpriseLibraryLoggerAdapter logger = new EnterpriseLibraryLoggerAdapter();
 
+        /// <summary>
+        ///     TODO The create logger.
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="ILoggerFacade" />.
+        /// </returns>
         protected override ILoggerFacade CreateLogger()
         {
-            return _logger;
+            return this.logger;
         }
     }
 }
--- LetsTalk/LetsTalk.Business.Managers/Managers/TelephonyManager.cs
+++ LetsTalk/LetsTalk.Business.Managers/Managers/TelephonyManager.cs
@@ -1,61 +1,125 @@
-﻿namespace LetsTalk.Business.Managers
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TelephonyManager.cs" company="GoDialog">
+//   All Rights Reserved
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace LetsTalk.Business.Managers
 {
+    #region Usings
+
     using System;
     using System.Collections.Generic;
     using System.ServiceModel;
 
     using LetsTalk.Business.Contracts;
 
-    using NServiceBus.Logging.Loggers;
+    #endregion
 
-    [ServiceBehavior(
-        InstanceContextMode = InstanceContextMode.Single, 
-        ConcurrencyMode = ConcurrencyMode.Multiple)]
+    /// <summary>
+    ///     TODO The telephony manager.
+    /// </summary>
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Multiple)]
     public class TelephonyManager : ManagerBase, ITelephonyService
     {
+        /// <summary>
+        ///     TODO The current channels.
+        /// </summary>
+        private static Dictionary<Guid, ITelephonyServiceCallbacks> currentChannels;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="TelephonyManager" /> class.
+        /// </summary>
         public TelephonyManager()
         {
-            CurrentChannels = new Dictionary<Guid, ITelephonyServiceCallbacks>();
+            currentChannels = new Dictionary<Guid, ITelephonyServiceCallbacks>();
         }
+
+        /// <summary>
+        /// TODO The close call.
+        /// </summary>
+        /// <param name="callId">
+        /// TODO The call id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
         public bool CloseCall(string callId)
         {
             return false;
         }
 
+        /// <summary>
+        /// TODO The connect.
+        /// </summary>
+        /// <param name="agentId">
+        /// TODO The agent id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
         public bool Connect(Guid agentId)
         {
             var s = OperationContext.Current.GetCallbackChannel<ITelephonyServiceCallbacks>();
-            CurrentChannels.Add(agentId,s);
+            currentChannels.Add(agentId, s);
             s.ConnectionSucceeded();
             return true;
         }
 
+        /// <summary>
+        ///     TODO The ping.
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="bool" />.
+        /// </returns>
         public bool Ping()
         {
-            //TODo:  remove manualy plased guid
-            CurrentChannels[Guid.Parse("D56F4395-3972-4CA9-9BDE-A4173B1EB051")].CallerConnect(new CallerInfo { CallerName = "Jonas deb grå", CallerNumber = 98608900, CallerId = Guid.NewGuid() });
+            // TODo:  remove manually placed guid
+            currentChannels[Guid.Parse("D56F4395-3972-4CA9-9BDE-A4173B1EB051")].CallerConnect(
+                new CallerInfo { CallerName = "Jonas deb grå", CallerNumber = 98608900, CallerId = Guid.NewGuid() });
             Console.WriteLine($"Got ping from {OperationContext.Current.Channel.RemoteAddress} at {DateTime.Now}");
             return true;
         }
 
+        /// <summary>
+        /// TODO The send call to.
+        /// </summary>
+        /// <param name="agentId">
+        /// TODO The agent id.
+        /// </param>
+        /// <param name="callInfo">
+        /// TODO The call info.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
         bool ITelephonyService.SendCallTo(Guid agentId, CallerInfo callInfo)
         {
             return SendCallTo(agentId, callInfo);
         }
 
-        private static Dictionary<Guid, ITelephonyServiceCallbacks> CurrentChannels; 
-
-        static bool SendCallTo(Guid agentId, CallerInfo callInfo)
+        /// <summary>
+        /// TODO The send call to.
+        /// </summary>
+        /// <param name="agentId">
+        /// TODO The agent id.
+        /// </param>
+        /// <param name="callInfo">
+        /// TODO The call info.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        private static bool SendCallTo(Guid agentId, CallerInfo callInfo)
         {
-            if (CurrentChannels.ContainsKey(agentId))
+            if (currentChannels.ContainsKey(agentId))
             {
                 ITelephonyServiceCallbacks callbacks;
-                var r =CurrentChannels.TryGetValue(agentId, out callbacks);
-                if (r != true)
-                    return false;
+                var r = currentChannels.TryGetValue(agentId, out callbacks);
+                if (r != true) return false;
                 callbacks.CallerConnect(callInfo);
                 return true;
             }
+
             return false;
         }
     }
