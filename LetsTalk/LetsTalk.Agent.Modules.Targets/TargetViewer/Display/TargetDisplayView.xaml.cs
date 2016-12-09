ad38aeee5bc0daa0e7eecca41f0c5e89cd677a12
Jonas Viktor fjeld <jvf_94@live.no>
2016-12-09 08:49:07 +0100

    Added some views from prototype

---
+++ LetsTalk/LetsTalk.Agent.Modules.Targets/TargetViewer/Display/TargetDisplayView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LetsTalk.Agent.Modules.Targets.TargetViewer.Display
+{
+    /// <summary>
+    /// Interaction logic for TargetDisplayView.xaml
+    /// </summary>
+    public partial class TargetDisplayView : UserControl
+    {
+        public TargetDisplayView()
+        {
+            InitializeComponent();
+        }
+    }
+}
